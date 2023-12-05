e7e3e8077d66b2ef6d34a513f53528b90b47c8b3
CodinaFJ <fjavier.codina@gmail.com>
2023-12-05 17:22:31 +0100

    LocalPosition change

--- Assets/Scripts/Agents/MoveToGoalAgent.cs
+++ Assets/Scripts/Agents/MoveToGoalAgent.cs
@@ -43,8 +43,8 @@ public class MoveToGoalAgent : Agent
 
 	public override void CollectObservations(VectorSensor sensor)
 	{
-		sensor.AddObservation((Vector2) this.transform.position);
-		sensor.AddObservation((Vector2) target.position);
+		sensor.AddObservation((Vector2) this.transform.localPosition);
+		sensor.AddObservation((Vector2) target.localPosition);
 	}
 
 	public override void Heuristic(in ActionBuffers actionsOut)
