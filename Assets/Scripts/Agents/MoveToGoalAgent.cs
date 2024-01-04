d6b91b7736945179ab0ca329f78bf2ece59117e4
Javier-Martin94 <65652422+Javier-Martin94@users.noreply.github.com>
2024-01-04 21:10:35 +0100

    punisment

--- Assets/Scripts/Agents/MoveToGoalAgent.cs
+++ Assets/Scripts/Agents/MoveToGoalAgent.cs
@@ -1,6 +1,7 @@
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveToGoalAgent : Agent
@@ -17,6 +18,8 @@ public class MoveToGoalAgent : Agent
 	private PlayerCollisions	playerCollisions;
 	private PlayerMovement		playerMovement;
 	private float				episodeTimeElapsed;
+	private float actionPunishmentReduction;
+	private int contadorAcciones;
 
 	protected void Start() 
 	{
@@ -56,6 +59,7 @@ public class MoveToGoalAgent : Agent
 	public override void OnEpisodeBegin()
 	{
 		episodeTimeElapsed = 0;
+		contadorAcciones = 0;  // JM: variable de test para ver cuantas acciones se hacen de media hasta alcanzar el objetivo
 		transform.localPosition = new Vector2(Random.Range(-7f, 7f), Random.Range(-1f, 0f));
 		target.transform.localPosition = new Vector2(Random.Range(-7.5f, 7.5f), Random.Range(-0.35f, -3f));
 	}
@@ -82,12 +86,17 @@ public class MoveToGoalAgent : Agent
 
 	public override void OnActionReceived(ActionBuffers actions)
 	{
+		contadorAcciones += 1;
 		float moveRight = actions.ContinuousActions[0];
 		float moveLeft = actions.ContinuousActions[1];
 		int jump = actions.DiscreteActions[0];
 
 		playerMovement.Movement = new Vector2(moveRight - moveLeft, playerMovement.Movement.y);
-		if (jump == 1) playerMovement.Jump();
+		if (jump == 1){
+			playerMovement.Jump();
+			AddReward(-0.1f); // Se da un pequeño punishment cada vez que salta para evitar que salte en exceso
+		} 
+		this.LogInfo("Numero de acciones: " + contadorAcciones);
 	}
 
 	private void RegisterCollisionsReactions()
@@ -100,10 +109,12 @@ public class MoveToGoalAgent : Agent
 
 	private void Reward()
 	{
+		actionPunishmentReduction = episodeTimeElapsed*-0.03f;
 		this.LogInfo("SUCCESS");
-		SetReward(goalReward);
+		AddReward(goalReward + actionPunishmentReduction); // Al valor de 10 por encontrar el objetivo le restamos un pequeño valor por cada accion realizada desde que empieza el episodio
 		winMask.SetActive(true);
 		loseMask.SetActive(false);
+		this.LogInfo("Total Reward: " + GetCumulativeReward()); // JM: Test para ver cuanto reward estan consiguiendo
 		EndEpisode();
 	}
 
@@ -113,6 +124,7 @@ public class MoveToGoalAgent : Agent
 		SetReward(wallPunishment * -1);
 		winMask.SetActive(false);
 		loseMask.SetActive(true);
+		this.LogInfo("Total Reward: " + GetCumulativeReward()); // JM: Test para ver cuanto reward estan consiguiendo
 		EndEpisode();
 	}
 
@@ -124,6 +136,7 @@ public class MoveToGoalAgent : Agent
 			bool win = value > 0;
 			winMask.SetActive(win);
 			loseMask.SetActive(!win);
+			this.LogInfo("Total Reward: " + GetCumulativeReward()); // JM: Test para ver cuanto reward estan consiguiendo
 			EndEpisode();
 		}
 	}
